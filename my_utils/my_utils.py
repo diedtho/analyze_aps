6d1fe12b6556ef64a52e6ae6dfd44df4c541e49d
Thorsten Diederichs <ddumdi11@gmail.com>
2022-10-24 21:47:02 +0200

    Verbesserungen, hot!

--- my_utils/my_utils.py
+++ my_utils/my_utils.py
@@ -2,48 +2,51 @@ import os
 from pathlib import Path
 
 
-def scan_directory_to_results_dict(path_string):
+def index_directory_subfolders(path_string):
     path = Path(path_string)
-    scan_result = [[path.name, 'root']]
-    scan_result_dict = {}
+    index_result_dict = {}
+    index_result_dict[path.name] = [str(path.absolute())]
     for entry in os.scandir(path):
         if entry.is_dir():
             try:
-                scan_result_dict_tmp = scan_directory_to_results_dict(entry)
+                index_result_dict_tmp = index_directory_subfolders(entry)
+                for key, val in index_result_dict_tmp.items():
+                    if key in index_result_dict.keys():
+                        list_old_tmp = index_result_dict[key].copy()
+                        list_new_tmp = val.copy()
+                        index_result_dict[key] = [*list_old_tmp, *list_new_tmp]
+                    else:
+                        index_result_dict[key] = [*val.copy()]
+            except:
+                pass
+    return index_result_dict
+
+def scan_directory_subfolders(path_string):
+    path = Path(path_string)
+    scan_result_dict = {}
+    scan_result_dict[str(path.absolute())] = [path.name, path.parts]
+    for entry in os.scandir(path):
+        if entry.is_dir():
+            try:
+                scan_result_dict_tmp = scan_directory_subfolders(entry)
                 scan_result_dict.update(scan_result_dict_tmp)
             except:
                 pass
-        else:
-            scan_result.append([entry.name, 'file'])
-    scan_result_dict[str(path)] = scan_result
     return scan_result_dict
-
-def scan_directory_to_results_list_folders(path_string):
-    path = Path(path_string)
-    scan_result_list = [path.name]
-    for entry in os.scandir(path):
-        if entry.is_dir():
-            try:
-                scan_result_list_tmp = scan_directory_to_results_list_folders(entry)
-                scan_result_list = scan_result_list + scan_result_list_tmp
-            except:
-                pass
-    return scan_result_list
 
 class RootFolder:
 
     def __init__(self, path_string):
         self.root_path = Path(path_string)
-        self.content_dict = {}
-        self.folders_list = []
+        self.folderpathes_dict = {}
+        self.folderindices_dict = {}
 
-    def get_subfolder(self):
-        self.folders_list = scan_directory_to_results_list_folders(self.root_path)
-        return self.folders_list
+    def get_subfolders(self):
+        if len(self.folderpathes_dict) == 0:
+            self.folderpathes_dict = scan_directory_subfolders(self.root_path)
+        return self.folderpathes_dict
 
-    def get_content_subfolder_by_name(self, subfolder_name):
-        return scan_directory_to_results_dict(self.root_path)
-
-    def get_content(self):
-        self.content_dict = scan_directory_to_results_dict(self.root_path)
-        return self.content_dict
+    def get_folderindices(self):
+        if len(self.folderindices_dict) == 0:
+            self.folderindices_dict = index_directory_subfolders(self.root_path)
+        return self.folderindices_dict
--- my_utils/try_my_utils_1.py
+++ my_utils/try_my_utils_1.py
@@ -1,12 +1,13 @@
 from pprint import pprint
-
 from my_utils import RootFolder
 
-pathstring = 'K:\CProg\Anlieferungen\EVA'
+# pathstring = 'K:\CProg\Anlieferungen\EVA'  # ComDev
+pathstring = '/home/zarko/AAA_Projektarbeit/'  # home linux
 
 rootdir = RootFolder(pathstring)
-subfolders = rootdir.get_subfolder()
+subfolders = rootdir.get_subfolders()
 print(subfolders)
-
-#content_root = rootdir.get_content()
-#pprint(content_root)
+print('=' * 111)
+subfolder_indices = rootdir.get_folderindices()
+for index, list in subfolder_indices.items():
+    print(index, list)
